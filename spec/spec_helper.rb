2828af62ff0defc8eff9f073f6d4810525b8406b
primordial <charles@jacksoncharles.co.uk>
2013-07-11 21:55:34 +0100

    End of chapter 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -42,4 +45,10 @@ RSpec.configure do |config|
 
   #Adding the Capybara DSL to the RSpec helper file. 
   config.include Capybara::DSL
+end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
