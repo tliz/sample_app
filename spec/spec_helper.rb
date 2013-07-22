c6c766989237deefb0d766f8f7eeeff27cc6208b
primordial <charles@jacksoncharles.co.uk>
2013-07-22 16:02:29 +0100

    chapter 9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,3 +52,17 @@ Spork.each_run do
   # This code will be run each time you run your specs.
 
 end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
